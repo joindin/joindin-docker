49e4bb5d23353e65cbf587d45ed9c2cc3050a02d
Jos Elstgeest <jos@tidi.nl>
2019-06-06 18:28:03 +0200

    update fixConfig script to set mailserver parameters

--- scripts/fixConfig.sh
+++ scripts/fixConfig.sh
@@ -2,5 +2,7 @@
 
 DOCKER_DIR=$PWD;
 
-sed -i 's/https:\/\/api.joind.in/http:\/\/api/' $DOCKER_DIR/joindin-web2/config/config.php
+sed -i 's/http:\/\/api.dev.joind.in/http:\/\/api/' $DOCKER_DIR/joindin-web2/config/config.php
 sed -i 's/127.0.0.1/redis/' $DOCKER_DIR/joindin-web2/config/config.php
+sed -i 's/localhost/mail/' $DOCKER_DIR/joindin-api/src/config.php
+sed -i 's/25/1025/' $DOCKER_DIR/joindin-api/src/config.php
